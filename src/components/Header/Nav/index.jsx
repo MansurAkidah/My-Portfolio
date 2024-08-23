0c5f64d1e02ff4ee1a8499662b4321e52b06049d
Mansur Akidah <mansurakidah@gmail.com>
2024-08-23 09:22:34 +0300

    Added a navigation and a projects list page with the necessary components

---
+++ src/components/Header/Nav/index.jsx
@@ -0,0 +1,58 @@
+'use client';
+import styles from './style.module.scss';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { height } from '../anim';
+import Body from './Body';
+import Footer from './Footer';
+import Image from './Image';
+
+const links = [
+  {
+    title: "Home",
+    href: "/",
+    src: "screenshot (233).png",
+    download: false
+  },
+  {
+    title: "About Me",
+    href: "/about",
+    src: "me.jpg",
+    download: false
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+    src: "screenshot (233).png",
+    download: false
+  },
+  {
+    title: "Contacts",
+    href: "/contact",
+    src: "screenshot (235).png",
+    download: false
+  },
+  {
+    title: "Resume",
+    href: "/mansur_resume.pdf",
+    src: "screenshot (247).png",
+    download: true
+  }
+]
+
+export default function Index() {
+
+  const [selectedLink, setSelectedLink] = useState({isActive: false, index: 0});
+
+  return (
+    <motion.div variants={height} initial="initial" animate="enter" exit="exit" className={styles.nav}>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <Body links={links} selectedLink={selectedLink} setSelectedLink={setSelectedLink}/>
+          <Footer />
+        </div>
+        <Image src={links[selectedLink.index].src} isActive={selectedLink.isActive}/>
+      </div>
+    </motion.div>
+  )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,44 @@
+'use client';
+import styles from './style.module.scss';
+import Link from 'next/link';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { opacity, background } from './anim';
+import Nav from './Nav';
+
+export default function index() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.bar}>
+                <Link href="/" className={styles.la}>Akidah</Link>
+                {/* <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}>Akidah</motion.p>
+                <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>Mansur</motion.p> */}
+                <div onClick={() => {setIsActive(!isActive)}} className={styles.el}>
+                    <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+                    <div className={styles.label}>
+                        <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}>Menu</motion.p>
+                        <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>Close</motion.p>
+                    </div>
+                </div>
+                <motion.div variants={opacity} animate={!isActive ? "open" : "closed"} className={styles.shopContainer}>
+                    {/* <p className={styles.shop}>Shop</p> */}
+                    <div className={styles.el}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-terminal" viewBox="0 0 16 16">
+                        <path d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9M3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708z"/>
+                        <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"/>
+                    </svg>
+                    <p><Link href="/projects">Projects</Link></p>
+                    {/* <Link href="/">Projects</Link> */}
+                    </div>
+                </motion.div>
+            </div>
+            <motion.div variants={background} initial="initial" animate={isActive ? "open" : "closed"} className={styles.background}></motion.div>
+            <AnimatePresence mode="wait">
+                {isActive && <Nav/>}
+            </AnimatePresence>
+        </div>
+    )
+}
