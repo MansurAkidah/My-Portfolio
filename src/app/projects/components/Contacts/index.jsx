c08ae65d63263c2cca59ed5b3272df19b65e39d6
Mansur Akidah <akidahmansur786@gmail.com>
2025-01-16 07:43:48 +0300

    changing case for deployment

--- src/app/projects/components/Contacts/index.jsx
+++ src/app/projects/components/Contacts/index.jsx
@@ -6,7 +6,7 @@ import { useScroll, motion, useTransform, useSpring } from 'framer-motion';
 import Magnetic from '../common/Magnetic/Index';
 
 //Contacts section
-export default function index() {
+export default function Index() {
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
         target: container,
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { opacity, background } from './anim';
 import Nav from './Nav';
 
-export default function index() {
+export default function Index() {
 
     const [isActive, setIsActive] = useState(false);
 
