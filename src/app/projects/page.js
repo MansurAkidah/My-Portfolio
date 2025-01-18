0ae6484cd00b37c5a569e4b08cc8f527cd39bc9a
Mansur Akidah <akidahmansur786@gmail.com>
2025-01-18 19:22:50 +0300

    Added the components and langing files

---
+++ src/app/projects/page.js
@@ -0,0 +1,43 @@
+'use client';
+import styles from './page.module.scss'
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion';
+// import Preloader from '../components/Preloader';
+import Landing from '../projects/components/Landing/index';
+// import Projects from '../projects/components/ProjectsList/Index';
+// import Description from '../projects/components/Description/Index';
+// import Contact from '../projects/components/Contacts/Index';
+// import SlidingImages from '../components/SlidingImages';
+
+export default function Index() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+
+          // setTimeout( () => {
+          //   setIsLoading(false);
+          //   document.body.style.cursor = 'default'
+          //   window.scrollTo(0,0);
+          // }, 2000)
+      }
+    )()
+  }, [])
+
+  return (
+    <main className={styles.main}>
+      {/* <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence> */}
+      <Landing />
+      {/* <Description />
+      <Projects /> */}
+      {/* <SlidingImages />*/}
+      {/* <Contact />  */}
+    </main>
+  )
+}
