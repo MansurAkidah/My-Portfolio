eec701c8d85def2c321d48cd7996a1a2e6f78e92
Mansur Akidah <akidahmansur786@gmail.com>
2025-01-19 21:11:19 +0300

    changed Index to index in roundedBotton (deployment)

--- src/app/projects/components/common/RoundedBottom/index.jsx
+++ src/app/projects/components/common/RoundedBottom/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect, useRef } from 'react';
 import styles from './style.module.scss';
 import gsap from 'gsap';
-import Magnetic from '../Magnetic/Index';
+import Magnetic from '../Magnetic/index';
 
 export default function Index({children, backgroundColor="#455CE9", ...attributes}) {
 
