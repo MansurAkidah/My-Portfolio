c55dcc9c70d1827a017f88f63e96f696971f3ac4
Mansur Akidah <akidahmansur786@gmail.com>
2025-01-19 21:08:05 +0300

    changed Index to index in page.js(deployment problems)

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 import { AnimatePresence } from 'framer-motion';
 // import Preloader from '../components/Preloader';
 import Landing from '../projects/components/Landing/index';
-import Description from '../projects/components/Description/Index';
+import Description from '../projects/components/Description/index';
 // import Projects from '../projects/components/ProjectsList/Index';
 // import Contact from '../projects/components/Contacts/index';
 // import SlidingImages from '../components/SlidingImages';
